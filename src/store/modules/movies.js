2ba401e4891d41c2fbce0b8244a4ae5cc30e40a8
Natalie <kalachevan615@gmail.com>
2020-09-25 17:58:06 +0700

    movies store state, getters, actions

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -1,16 +1,28 @@
+import IDs from "@/store/mock/imdb";
 import axios from "@/plugins/axios";
 
 const moviesStore = {
     namespaced: true,
-    state: {},
-    getters: {},
+    state: {
+        top250: IDs,
+        perPage: 12,
+        curPage: 1
+    },
+    getters: {
+        slicedIds: ({ top250 }) => (from, to) => top250.slice(from, to),
+        curPage: ({ curPage }) => curPage,
+        perPage: ({ perPage }) => perPage
+    },
     mutations: {},
     actions: {
-        async fetchMovies(context) {
-            console.log(context);
-            //tt3896198
-
-            const response = await axios.get('/?i=tt3896198')
+        async fetchMovies({ getters }) {
+            const { curPage, perPage, slicedIds } = getters;
+            const from = (curPage * perPage) - perPage;
+            const to = curPage * perPage;
+            const moviesToFetch = slicedIds(from, to);
+            console.log(moviesToFetch);
+            const requests = moviesToFetch.map((id) => axios.get(`/?i=${id}`)); 
+            const response = await Promise.all(requests);
             console.log(response); 
         }
     }
