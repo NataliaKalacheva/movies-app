5361e1058106a16d135a09eb907e16e231a192f0
Natalie <kalachevan615@gmail.com>
2020-10-27 13:53:33 +0700

    loader finished

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,5 +1,9 @@
 <template>
- <div>Loader</div>
+ <div class="loader__wrapper" v-if="isShowLoader">
+    <div>
+     <b-spinner type="grow" label="Loading..."></b-spinner>
+    </div>
+ </div>
 </template>
 
 <script>
@@ -8,8 +12,20 @@ import { mapGetters } from "vuex";
 export default {
     name: "Loader",
     computed: {
-        ...mapGetters("loader", ["isShowLoader"])
+        ...mapGetters(["isShowLoader"])
     }
 }
 
 </script>
+
+<style lang="scss" scoped>
+.loader__wrapper {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 99;
+}
+</style>
